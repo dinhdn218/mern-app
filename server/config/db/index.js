67aeac8297989d9674a667c719986a1f7a55b4ce
dinhdn@sunteco <dinhdn@sunteco>
2025-01-08 15:56:28 +0700

    add Dockerfile

--- server/config/db/index.js
+++ server/config/db/index.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DB_URL)
-    console.log('connect success')
+    await mongoose.connect(process.env.DB_URL);
+    console.log("connect success");
   } catch (error) {
-    console.log('connect fail')
+    console.log("connect fail", error);
   }
 }
 
-module.exports = { connect }
+module.exports = { connect };
